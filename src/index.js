260ad7e72415a518bed1e9ab3e713ced7328de73
Ilia Rebenok <rebenok.ilya13@gmail.com>
2023-05-02 18:00:42 +0700

    deeleted age verification page

--- src/index.js
+++ src/index.js
@@ -3,25 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {createBrowserRouter, RouterProvider, useRoutes} from 'react-router-dom';
-import AgeVerificationMiddleware from "./middlewares/AgeVerificationMiddleware";
-import AgeVerification from "./components/shared/AgeVerification";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: (
-            <AgeVerificationMiddleware>
+            
                 <App/>
-            </AgeVerificationMiddleware>
-        ),
-    },
-    {
-        path: '/age-verification',
-        element: (
-            <>
-                <App/>
-                <AgeVerification/>
-            </>
+            
         ),
     },
 ]);
