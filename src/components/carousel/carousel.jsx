dbe19e51f87ccee99f18c1fc737c5a4f6d26bc0d
Ilia Rebenok <rebenok.ilya13@gmail.com>
2023-05-02 23:45:18 +0700

    polished  cross browser

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,9 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from './carousel.module.css'
-import logo from '../../images/meetweedLogo/MEETWEED TH-横向LOGO 绿标白字.png'
-import {IMAGES} from '../../assets/imagesCarousel'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { IMAGES } from '../../assets/imagesCarousel'
 
 function Carousel() {
 
@@ -20,48 +17,38 @@ function Carousel() {
     }
 
     return (
-        <>
-            <div className={styles.wrap}>
-                {IMAGES.map((image, ind) => {
-                    console.log(IMAGES[urlIndex].src)
-                    return (
-                        <div
-                            key={image.id}
+        <div className={styles.wrap}>
+            {IMAGES.map((image, ind) => {
+                console.log(IMAGES[urlIndex].src)
+                return (
+                    <div key={image.id}>
+                        <img
+                            src={IMAGES[urlIndex].src}
+                            alt={IMAGES[urlIndex].name}
                             className={urlIndex === ind ? `${styles.activeImage} ${styles.image} ` : `${styles.image}`}
-                        >
-                            <LazyLoadImage 
-                                src={IMAGES[urlIndex].src} 
-                                alt={IMAGES[urlIndex].name} 
-                                height='200px'
-                                width='500px'
-                                effect='blur'
-                            />
-                        </div>
-                    )
-                })}
-                {IMAGES.map((image, ind) => {
-                    return (
-                        <div
-                            key={image.id}
-                            className={urlIndex === ind ? `${styles.image} ${styles.imageTwo} ${styles.activeImage}` : `${styles.image} ${styles.imageTwo}`}
-                        >
-                            <LazyLoadImage 
-                                src={urlIndex === IMAGES.length - 1 ? IMAGES[0].src : IMAGES[urlIndex + 1].src}
-                                alt={urlIndex === IMAGES.length - 1 ? IMAGES[0].name : IMAGES[urlIndex + 1].name}
-                                height='200px'
-                                width='500px'
-                                effect='blur'
-                            />
-                        </div>
-                    )
-                })}
-                <div className={styles.buttonWrap}>
-                    <button className={`${styles.button} ${styles.buttonBack}`} onClick={clickPrevious} />
-                    <button className={styles.button} onClick={clickNext} />
-                </div>
-            </div>
-        </>
+                        />
+                    </div>
+                )
+            })}
+            {IMAGES.map((image, ind) => {
+                return (
+                    <div
+                        key={image.id}
 
+                    >
+                        <img
+                            src={urlIndex === IMAGES.length - 1 ? IMAGES[0].src : IMAGES[urlIndex + 1].src}
+                            alt={urlIndex === IMAGES.length - 1 ? IMAGES[0].name : IMAGES[urlIndex + 1].name}
+                            className={urlIndex === ind ? `${styles.image} ${styles.imageTwo} ${styles.activeImage}` : `${styles.image} ${styles.imageTwo}`}
+                        />
+                    </div>
+                )
+            })}
+            <div className={styles.buttonWrap}>
+                <button className={`${styles.button} ${styles.buttonBack}`} onClick={clickPrevious} />
+                <button className={styles.button} onClick={clickNext} />
+            </div>
+        </div>
     )
 }
 
--- src/components/featuredCard/featuredCard.jsx
+++ src/components/featuredCard/featuredCard.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import styles from './featuredCard.module.css'
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+
 
 
 function FeaturedCard(props) {
   return (
     <div className={styles.wrap}>
       <div className={styles.image}>
-      <LazyLoadImage
-        height='160px'
-        width='160px'
-        effect='blur'
+      <img
         className={styles.image} src={props.image} alt='weed bud' />
         </div>
       <h3 className={styles.productName}> {props.productName} </h3>
